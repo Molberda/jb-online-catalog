e7d60274ee885da21837bf21e10e018110671662
David <davidmollera@hotmail.com>
2022-12-29 18:30:37 -0500

    installed and imported the library for font awesome icons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {  } from '@fortawesome/free-solid-svg-icons'
+
+library.add(  )
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
