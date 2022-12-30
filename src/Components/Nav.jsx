a121cd0f1d9c97ff85fa99e7c09bd22ace8c3c23
David <davidmollera@hotmail.com>
2022-12-29 22:00:56 -0500

    added code and css for the nav elements and created an assets folder with the logo and pedidos imgs

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,17 +1,20 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import Logo from "../Assets/logo.png"
 
 const Nav = () => {
-    return (
-        <nav className='nav column'>
-            <figure className="nav__logo--wrapper">
-                <img src="" alt="" className="nav__logo" />
-            </figure>
-            <ul>
-            <FontAwesomeIcon icon="fa-solid fa-jar-wheat" />
-            </ul>
-        </nav>
-    );
-}
+  return (
+    <nav className="nav column">
+      <figure className="nav__logo--wrapper">
+        <img src={Logo} alt="" className="nav__logo" />
+      </figure>
+      <ul>
+        <li className="nav__link--item">
+          <FontAwesomeIcon icon="fa-solid fa-jar-wheat" />
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Nav;
