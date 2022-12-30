0fb03e6f77cae120a24525e63372d438eb638953
David <davidmollera@hotmail.com>
2022-12-29 19:14:34 -0500

    imported browserrouter,routes and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    </BrowserRouter>
   );
 }
 
