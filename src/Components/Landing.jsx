7c104c981e0e2737add43259a415630e51b15bf9
David <davidmollera@hotmail.com>
2022-12-30 21:40:23 -0500

    added css properties for landing title and also imported a new font and created a landing text box div

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import Logo from '../Assets/logo.png'
+import React from "react";
+import Logo from "../Assets/logo.png";
 
 const Landing = () => {
-    return (
-        <section id='landing'>
-            <div className="container landing__container">
-                <div className="row landing__row column">
-                    <figure className="landing__img--wrapper">
-                        <img src={Logo} alt="" className="landing__img" />
-                    </figure>
-                    <h1 className="landing__title">DELIZZIOZO</h1>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section id="landing">
+      <div className="container landing__container">
+        <div className="row landing__row column">
+          <figure className="landing__img--wrapper">
+            <img src={Logo} alt="" className="landing__img" />
+          </figure>
+          <div className="landing__textbox">
+            <h1 className="landing__title">DELIZZIOZO</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Landing;
