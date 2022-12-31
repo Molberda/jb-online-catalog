8dbd9148821d3adf4b351767731a08943775d47c
David <davidmollera@hotmail.com>
2022-12-31 15:19:29 -0500

    used the arrow down in the landing page

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -13,6 +13,7 @@ const Landing = () => {
             <h1 className="landing__title">DELIZZIOZO</h1>
             <h2 className="landing__subtitle">Todos tus antojitos en un solo lugar</h2>
           </div>
+          <FontAwesomeIcon icon="fa-solid fa-angles-down" />
         </div>
       </div>
     </section>
