491daa7d9da20f5ae9c1ab375ae00b5e7c1b45a4
David <davidmollera@hotmail.com>
2022-12-30 19:54:07 -0500

    added the initial code for the landing section

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Landing = () => {
     return (
-        <div>
-            
-        </div>
+        <section id='landing'>
+            <div className="container landing__container">
+                <div className="row landing__row column">
+                    <figure className="landing__img--wrapper">
+                        <img src="" alt="" className="landing__img" />
+                    </figure>
+                </div>
+            </div>
+        </section>
     );
 }
 
