666da76e3a454d560d095f7e4faca592a8e31ca8
David <davidmollera@hotmail.com>
2023-01-01 13:52:20 -0500

    used a link for the img for antipasto section

--- src/Components/Antipasto.jsx
+++ src/Components/Antipasto.jsx
@@ -6,7 +6,7 @@ const Antipasto = () => {
             <div className="container antipasto__container">
                 <div className="row antipasto__row">
                     <figure className="antipasto__img--wrapper img__wrapper">
-                        <img src="" alt="" className="antipasto__img" />
+                        <img src="https://mercaldas.vtexassets.com/arquivos/ids/155561-800-auto?v=636885393807930000&width=800&height=auto&aspect=true" alt="" className="antipasto__img" />
                     </figure>
                 </div>
             </div>
