6b6ab7ed64b16d30faeed094b0cd8459a574e7ee
David <davidmollera@hotmail.com>
2023-01-02 10:58:26 -0500

    added more html code for the postres section

--- src/Components/Postres.jsx
+++ src/Components/Postres.jsx
@@ -6,9 +6,11 @@ const Postres = () => {
             <div className="container postres__container">
                 <div className="row postres__row column">
                     <figure className="postres__img--wrapper img__wrapper">
-                        <img src="" alt="" className="postres__img" />
+                        <img src="https://t2.uc.ltmcdn.com/es/posts/4/4/2/como_hacer_postre_de_milo_51244_orig.jpg" alt="" className="postres__img" />
                     </figure>
-                    <h2 className="title postres__title">POSTRE DE LIMON</h2>
+                    <h2 className="title postres__title">POSTRE DE MILO</h2>
+                    <h2 className="subtitle postres__subtitle">Nuestra opción que calmará todos tus antojos de azucar</h2>
+                    <h2 className="text postres__text">Un delicioso postre con leche condensada, crema de leche, galletas Ducales y Milo</h2>
                 </div>
             </div>
         </section>
