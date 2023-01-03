c1548aedb685f4935794afb1ef17c8573fd78d1c
David <davidmollera@hotmail.com>
2023-01-03 18:21:59 -0500

    changed the class of nav link item to nav list item bc it makes more sense

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -9,25 +9,25 @@ const Nav = () => {
         <img src={Logo} alt="" className="nav__logo" />
       </figure>
       <ul>
-        <li className="nav__link--item">
+        <li className="nav__list--item">
           <a href="#ceviche" className="nav__link">
             <FontAwesomeIcon icon="fa-solid fa-jar-wheat" />
             <span className="nav__name">Ceviche</span>
           </a>
         </li>
-        <li className="nav__link--item">
+        <li className="nav__list--item">
           <a href="#antipasto" className="nav__link">
             <FontAwesomeIcon icon="fa-solid fa-leaf" />
             <span className="nav__name">Antipasto</span>
           </a>
         </li>
-        <li className="nav__link--item">
+        <li className="nav__list--item">
           <a href="#paella" className="nav__link">
           <FontAwesomeIcon icon="fa-solid fa-utensils" />
             <span className="nav__name">Paella</span>
           </a>
         </li>
-        <li className="nav__link--item">
+        <li className="nav__list--item">
           <a href="#postres" className="nav__link">
             <FontAwesomeIcon icon="fa-solid fa-cookie-bite" />
             <span className="nav__name">Postres</span>
