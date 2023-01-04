2dfc63ea8222780626a12737d75e2cb47082203e
David <davidmollera@hotmail.com>
2023-01-03 22:45:23 -0500

    added more html for paella jsx and imported a paella img

--- src/Components/Paella.jsx
+++ src/Components/Paella.jsx
@@ -5,7 +5,9 @@ const Paella = () => {
         <section id="paella">
             <div className="container paella__container">
                 <div className="row paella__row">
-                    
+                    <figure className="paella__img--wrapper img__wrapper">
+                        <img src="" alt="" className="paella__img" />
+                    </figure>
                 </div>
             </div>
         </section>
