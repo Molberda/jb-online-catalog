527f8263c4eba6ce179f42d9d99f2dec79221b3b
David <davidmollera@hotmail.com>
2023-01-04 10:11:47 -0500

    imported the envelope icon to the library

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faJarWheat, faLeaf, faCookieBite, faAnglesDown, faUtensils } from '@fortawesome/free-solid-svg-icons'
+import { faJarWheat, faLeaf, faCookieBite, faAnglesDown, faUtensils, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
-library.add( faJarWheat, faLeaf, faCookieBite, faAnglesDown, faUtensils )
+library.add( faJarWheat, faLeaf, faCookieBite, faAnglesDown, faUtensils, faEnvelope )
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
