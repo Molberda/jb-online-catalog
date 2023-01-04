a74e926db0c6fb8a7d0f883fdd81306a50a5b087
David <davidmollera@hotmail.com>
2023-01-03 22:40:29 -0500

    added the initial html for paella section

--- src/Components/Paella.jsx
+++ src/Components/Paella.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Paella = () => {
     return (
-        <div>
-            
-        </div>
+        <section id="paella">
+            <div className="container paella__container">
+                <div className="row paella__row">
+                    
+                </div>
+            </div>
+        </section>
     );
 }
 
