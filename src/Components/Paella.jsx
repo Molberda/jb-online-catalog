e4127af1f86fe01b3e0d1ac4667823bb18330a99
David <davidmollera@hotmail.com>
2023-01-03 22:59:32 -0500

    added title and subtitle tags to the paella component

--- src/Components/Paella.jsx
+++ src/Components/Paella.jsx
@@ -5,10 +5,12 @@ const Paella = () => {
     return (
         <section id="paella">
             <div className="container paella__container">
-                <div className="row paella__row">
+                <div className="row paella__row column">
                     <figure className="paella__img--wrapper img__wrapper">
                         <img src={paellaImg} alt="" className="paella__img" />
                     </figure>
+                    <h2 className="title paella__title">PAELLA</h2>
+                    <h2 className="subtitle paella__subtitle"></h2>
                 </div>
             </div>
         </section>
