ac1982e13a12a8fee000771ce57b9ca03ef93c30
David <davidmollera@hotmail.com>
2023-01-04 10:13:05 -0500

    imported font awesome icon to contactanos component

--- src/Components/Contactanos.jsx
+++ src/Components/Contactanos.jsx
@@ -1,9 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const Contactanos = () => {
     return (
         <button className="contact__btn">
-            
+            <FontAwesomeIcon icon="fa-regular fa-envelope" />
         </button>
     );
 }
