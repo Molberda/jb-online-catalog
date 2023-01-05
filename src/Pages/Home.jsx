c6528631c62e345bc44cd658a836c553d42e42ce
David <davidmollera@hotmail.com>
2023-01-05 11:46:24 -0500

    imported the components to the home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Antipasto from '../Components/Antipasto';
+import Ceviche from '../Components/Ceviche';
+import Landing from '../Components/Landing';
+import Paella from '../Components/Paella';
+import Postres from '../Components/Postres';
 
 const Home = () => {
     return (
         <div>
-            
+            <Landing/>
+            <Ceviche/>
+            <Antipasto/>
+            <Paella/>
+            <Postres/>
         </div>
     );
 }
