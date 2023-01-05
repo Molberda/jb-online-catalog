e20166ac8b43f2cbd5b5bf249abc82abb4e40f63
David <davidmollera@hotmail.com>
2023-01-05 12:29:20 -0500

    created a route to display the contact page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./Components/Footer";
 import Nav from "./Components/Nav";
+import Contact from "./Pages/Contact";
 import Home from "./Pages/Home";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/contact" element={<Contact/>}/>
         </Routes>
         <Footer />
       </Router>
