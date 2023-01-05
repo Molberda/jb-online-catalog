f0fce513cfd44e3fcfe9c685e47446a199cb3e8e
David <davidmollera@hotmail.com>
2023-01-05 13:12:04 -0500

    added more html and css to contact section

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import Pedidos from '../Assets/pedidos.png'
+import React from "react";
+import Pedidos from "../Assets/pedidos.png";
 
 const Contact = () => {
-    return (
-        <section id="contact">
-            <div className="container contact__container">
-                <div className="row container__row">
-                    <figure className="contact__img--wrapper">
-                        <img src={Pedidos} alt="" className="contact__img" />
-                    </figure>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section id="contact">
+      <div className="container contact__container">
+        <div className="row container__row">
+          <figure className="contact__img--wrapper">
+            <img src={Pedidos} alt="" className="contact__img" />
+          </figure>
+          <div className="textbox contact__textbox">
+            <h2 className="title contact__title">¡Contactanos!</h2>
+            <h2 className="subtitle contact__subtitle">Siguenos en instagram <a href="/" className="subtitle contact__subtitle igLink">@Deliziozo</a> <br /> O escribenos directamente a Whatsapp</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Contact;
