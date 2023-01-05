3411dafe014cc9705a72959fd7393122dc38a542
David <davidmollera@hotmail.com>
2023-01-05 11:04:48 -0500

    added an anchor tag to the contactanos component

--- src/Components/Contactanos.jsx
+++ src/Components/Contactanos.jsx
@@ -1,12 +1,14 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
 const Contactanos = () => {
-    return (
-        <button className="contact__btn">
-            <FontAwesomeIcon icon="fa-regular fa-envelope" />
-        </button>
-    );
-}
+  return (
+    <a href="">
+      <button className="contact__btn">
+        <FontAwesomeIcon icon="fa-regular fa-envelope" />
+      </button>
+    </a>
+  );
+};
 
 export default Contactanos;
