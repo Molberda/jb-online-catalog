afeeffaced462acb3f90449e2a2cca0c15ebe31c
David <davidmollera@hotmail.com>
2023-01-09 20:25:14 -0500

    added a buy btn to the anitrpasto section wrapped in a link that will take you to contact page

--- src/Components/Antipasto.jsx
+++ src/Components/Antipasto.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Antipasto = () => {
-    return (
-        <section id="antipasto">
-            <div className="container antipasto__container">
-                <div className="row antipasto__row column">
-                    <figure className="antipasto__img--wrapper img__wrapper">
-                        <img src="https://tomacol.co/wp-content/uploads/2022/01/Antipasto-champinones-120g.png" alt="" className="antipasto__img" />
-                    </figure>
-                    <h2 className="title antipasto__title">ANTIPASTO</h2>
-                    <h2 className="subtitle antipasto__subtitle">Un acompañante exquisito y unico</h2>
-                    <h2 className="text antipasto__text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam id iure cum unde amet voluptates.</h2>
-                    <h2 className="price antipasto__price text">$20.000</h2>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section id="antipasto">
+      <div className="container antipasto__container">
+        <div className="row antipasto__row column">
+          <figure className="antipasto__img--wrapper img__wrapper">
+            <img
+              src="https://tomacol.co/wp-content/uploads/2022/01/Antipasto-champinones-120g.png"
+              alt=""
+              className="antipasto__img"
+            />
+          </figure>
+          <h2 className="title antipasto__title">ANTIPASTO</h2>
+          <h2 className="subtitle antipasto__subtitle">
+            Un acompañante exquisito y unico
+          </h2>
+          <h2 className="text antipasto__text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam id
+            iure cum unde amet voluptates.
+          </h2>
+          <h2 className="price antipasto__price text">$20.000</h2>
+          <Link to="/contact">
+            <button className="buy__btn">Pidelo Ya</button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Antipasto;
