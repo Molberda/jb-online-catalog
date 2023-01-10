02a4a2439a9a9f8111dcc2586b603f5d15ae0480
David <davidmollera@hotmail.com>
2023-01-09 20:26:24 -0500

    added buy btn to the postres section

--- src/Components/Postres.jsx
+++ src/Components/Postres.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Postres = () => {
-    return (
-        <section id="postres">
-            <div className="container postres__container">
-                <div className="row postres__row column">
-                    <figure className="postres__img--wrapper img__wrapper">
-                        <img src="https://t2.uc.ltmcdn.com/es/posts/4/4/2/como_hacer_postre_de_milo_51244_orig.jpg" alt="" className="postres__img" />
-                    </figure>
-                    <h2 className="title postres__title">POSTRE DE MILO</h2>
-                    <h2 className="subtitle postres__subtitle">Nuestra opción que calmará todos tus antojos de azucar</h2>
-                    <h2 className="text postres__text">Un delicioso postre con leche condensada, crema de leche, galletas Ducales y Milo</h2>
-                    <h2 className="price postres__price text">$15.000</h2>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section id="postres">
+      <div className="container postres__container">
+        <div className="row postres__row column">
+          <figure className="postres__img--wrapper img__wrapper">
+            <img
+              src="https://t2.uc.ltmcdn.com/es/posts/4/4/2/como_hacer_postre_de_milo_51244_orig.jpg"
+              alt=""
+              className="postres__img"
+            />
+          </figure>
+          <h2 className="title postres__title">POSTRE DE MILO</h2>
+          <h2 className="subtitle postres__subtitle">
+            Nuestra opción que calmará todos tus antojos de azucar
+          </h2>
+          <h2 className="text postres__text">
+            Un delicioso postre con leche condensada, crema de leche, galletas
+            Ducales y Milo
+          </h2>
+          <h2 className="price postres__price text">$15.000</h2>
+          <Link to="/contact">
+            <button className="buy__btn">Pidelo Ya</button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Postres;
