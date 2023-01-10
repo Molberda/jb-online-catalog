f9315e0f48d710fb9135274796c931fa33d3385b
David <davidmollera@hotmail.com>
2023-01-09 20:25:55 -0500

    added tthe buy btn to the paella section

--- src/Components/Paella.jsx
+++ src/Components/Paella.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import paellaImg from '../Assets/paella.jpg'
+import React from "react";
+import { Link } from "react-router-dom";
+import paellaImg from "../Assets/paella.jpg";
 
 const Paella = () => {
-    return (
-        <section id="paella">
-            <div className="container paella__container">
-                <div className="row paella__row column">
-                    <figure className="paella__img--wrapper img__wrapper">
-                        <img src={paellaImg} alt="" className="paella__img" />
-                    </figure>
-                    <h2 className="title paella__title">PAELLA</h2>
-                    <h2 className="subtitle paella__subtitle">Nuestro plato fuerte insignia</h2>
-                    <h2 className="text paella__text">Una paella que te transportara directamente a España <br /> Hecha con los ingredientes que más te gustan, frescos y preparados a la perfección</h2>
-                    <h2 className="price paella__price text">$35.000 (cada plato)</h2>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <section id="paella">
+      <div className="container paella__container">
+        <div className="row paella__row column">
+          <figure className="paella__img--wrapper img__wrapper">
+            <img src={paellaImg} alt="" className="paella__img" />
+          </figure>
+          <h2 className="title paella__title">PAELLA</h2>
+          <h2 className="subtitle paella__subtitle">
+            Nuestro plato fuerte insignia
+          </h2>
+          <h2 className="text paella__text">
+            Una paella que te transportara directamente a España <br /> Hecha
+            con los ingredientes que más te gustan, frescos y preparados a la
+            perfección
+          </h2>
+          <h2 className="price paella__price text">$35.000 (cada plato)</h2>
+          <Link to="/contact">
+            <button className="buy__btn">Pidelo Ya</button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Paella;
