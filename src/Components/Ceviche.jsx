c857f506d9297c6c7c62c1c810faf657239ae2c5
David <davidmollera@hotmail.com>
2023-01-09 20:24:20 -0500

    added a buy btn to the ceviche section wrapped in a link that will take you to contact page

--- src/Components/Ceviche.jsx
+++ src/Components/Ceviche.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Ceviche = () => {
   return (
@@ -12,6 +13,9 @@ const Ceviche = () => {
           <h2 className="subtitle ceviche__subtitle">Una entrada imperdible y para toda la familia</h2>
           <h2 className="text ceviche__text">La especialidad de la casa.<br /> Un ceviche de champiñiones bañado en salsa de tomate, limon & picante. <br /> ¡Sirvelo con tostadas o galletas! </h2>
           <h2 className="price ceviche__price text">$22.500</h2>
+          <Link to='/contact'>
+          <button className="buy__btn">Pidelo Ya</button>
+          </Link>
         </div>
       </div>
     </section>
