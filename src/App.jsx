ffdc0aa15ba4e799f33af0cc4305760a82f99071
David <davidmollera@hotmail.com>
2023-01-13 18:49:44 -0500

    imported the burger component to the app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import Footer from "./Components/Footer";
 import Contact from "./Pages/Contact";
 import Home from "./Pages/Home";
+import Burger from "./Pages/Ui/Burger";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Burger/>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/contact" element={<Contact/>}/>
