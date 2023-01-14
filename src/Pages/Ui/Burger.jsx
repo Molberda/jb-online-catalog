15750451e60b8cd989555b452099c7d3490a6595
David <davidmollera@hotmail.com>
2023-01-13 19:40:06 -0500

    defined the close menu funtion and used the links to call it

--- src/Pages/Ui/Burger.jsx
+++ src/Pages/Ui/Burger.jsx
@@ -3,18 +3,19 @@ import { Link } from "react-router-dom";
 
 const Burger = () => {
     function closeMenu(){
-
+        document.body.classList.remove('open__menu')
+        console.log('log')
     }
   return (
     <div className="bars">
       <ul className="bars__list">
         <li className="bars__item">
-          <Link to="/home" onClick={closeMenu}>
+          <Link to="/" onClick={closeMenu}>
             <h3 className="bars__link">Pagina Principal</h3>
           </Link>
         </li>
         <li className="bars__item">
-          <Link to="/contact" onClick={closeMenu()}>
+          <Link to="/contact" onClick={closeMenu}>
             <h3 className="bars__link">Contactanos</h3>
           </Link>
         </li>
