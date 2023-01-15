26733dbb996d44d6771d770220b182c704586a3e
David <davidmollera@hotmail.com>
2023-01-14 23:45:25 -0500

    accepted the name1 and icon1 props for the navbar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../Assets/logo.png";
 
-const Nav = () => {
+const Nav = ({name1, link1}) => {
   function openMenu() {
     document.body.classList += " open__menu";
     console.log("open");
@@ -45,6 +45,42 @@ const Nav = () => {
         </li>
       </ul>
     </nav>
+    // <nav className="nav column">
+    //   <a href="#landing">
+    //     <figure className="nav__logo--wrapper">
+    //       <img src={Logo} alt="" className="nav__logo" />
+    //     </figure>
+    //   </a>
+    //   <button className="bars__btn" onClick={openMenu}>
+    //     <FontAwesomeIcon icon="fa-solid fa-bars" />
+    //   </button>
+    //   <ul>
+    //     <li className="nav__list--item">
+    //       <a href="#ceviche" className="nav__link">
+    //         <FontAwesomeIcon icon="fa-solid fa-jar-wheat" />
+    //         <span className="nav__name">Ceviche</span>
+    //       </a>
+    //     </li>
+    //     <li className="nav__list--item">
+    //       <a href="#antipasto" className="nav__link">
+    //         <FontAwesomeIcon icon="fa-solid fa-leaf" />
+    //         <span className="nav__name">Antipasto</span>
+    //       </a>
+    //     </li>
+    //     <li className="nav__list--item">
+    //       <a href="#paella" className="nav__link">
+    //         <FontAwesomeIcon icon="fa-solid fa-utensils" />
+    //         <span className="nav__name">Paella</span>
+    //       </a>
+    //     </li>
+    //     <li className="nav__list--item">
+    //       <a href="#postres" className="nav__link">
+    //         <FontAwesomeIcon icon="fa-solid fa-cookie-bite" />
+    //         <span className="nav__name">Postres</span>
+    //       </a>
+    //     </li>
+    //   </ul>
+    // </nav>
   );
 };
 
