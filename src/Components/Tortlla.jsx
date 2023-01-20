74bfb4f7ddf0e3e0b443113c90aa2a87c46daf41
David <davidmollera@hotmail.com>
2023-01-20 11:22:09 -0500

    added a buy__btn class for the tortilla section

--- src/Components/Tortlla.jsx
+++ src/Components/Tortlla.jsx
@@ -1,21 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const Tortlla = () => {
-    return (
-       <section id="tortilla">
-        <div className="container tortilla__container">
-            <div className="row tortilla__row column">
-                <figure className="img__wrapper tortilla__img--wrapper">
-                    <img src="" alt="" className="tortilla__img" />
-                </figure>
-                <h2 className="title tortilla__title">TORTILLA</h2>
-                <h2 className="subtitle tortilla__subtitle">Perfecto para cualquier hora del dia</h2>
-                <h2 className="text tortilla text">¡Una tortilla de patatas en su punto! <br /> Acompañala con mayonesa o sirvela sola.</h2>
-                <h2 className="price text tortilla__price">$30.000</h2>
-            </div>
+  return (
+    <section id="tortilla">
+      <div className="container tortilla__container">
+        <div className="row tortilla__row column">
+          <figure className="img__wrapper tortilla__img--wrapper">
+            <img src="" alt="" className="tortilla__img" />
+          </figure>
+          <h2 className="title tortilla__title">TORTILLA</h2>
+          <h2 className="subtitle tortilla__subtitle">
+            Perfecto para cualquier hora del dia
+          </h2>
+          <h2 className="text tortilla text">
+            ¡Una tortilla de patatas en su punto! <br /> Acompañala con mayonesa
+            o sirvela sola.
+          </h2>
+          <h2 className="price text tortilla__price">$30.000</h2>
+          <Link to="/contact">
+            <button className="buy__btn">Pidelo Ya</button>
+          </Link>
         </div>
-       </section>
-    );
-}
+      </div>
+    </section>
+  );
+};
 
 export default Tortlla;
