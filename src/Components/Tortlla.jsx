19eab58b21f6fbca99e4219375ed90bb15ac0ee2
David <davidmollera@hotmail.com>
2023-01-19 20:33:59 -0500

    added html code for the tortilla img and the tortilla img wrapper

--- src/Components/Tortlla.jsx
+++ src/Components/Tortlla.jsx
@@ -4,7 +4,11 @@ const Tortlla = () => {
     return (
        <section id="tortilla">
         <div className="container tortilla__container">
-            <div className="row tortilla__row column"></div>
+            <div className="row tortilla__row column">
+                <figure className="img__wrapper tortilla__img--wrapper">
+                    <img src="" alt="" className="tortilla__img" />
+                </figure>
+            </div>
         </div>
        </section>
     );
