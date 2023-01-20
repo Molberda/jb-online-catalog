bf3430145e24cc7d0afae913d8a302901a74772d
David <davidmollera@hotmail.com>
2023-01-19 23:30:14 -0500

    created a price tag for the tortilla section

--- src/Components/Tortlla.jsx
+++ src/Components/Tortlla.jsx
@@ -11,6 +11,7 @@ const Tortlla = () => {
                 <h2 className="title tortilla__title">TORTILLA</h2>
                 <h2 className="subtitle tortilla__subtitle">Perfecto para cualquier hora del dia</h2>
                 <h2 className="text tortilla text">¡Una tortilla de patatas en su punto! <br /> Acompañala con mayonesa o sirvela sola.</h2>
+                <h2 className="price text tortilla__price">$30.000</h2>
             </div>
         </div>
        </section>
